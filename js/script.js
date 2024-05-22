60a1f2984b1cfd461d01168c9dff00679049fd4a
ihsanalfauzi <ihsanalfauzi99@gmail.com>
2024-05-22 23:58:49 +0700

    membuat function untuk menampilkan form hitung

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// JavaScript Kalkulator Segitiga
+// Function dan Ambil ID dari Form
+function tampilForm(type) {
+  const formLuas = document.getElementById("form-luas");
+  const formKeliling = document.getElementById("form-keliling");
+
+  // Jika Tombol di Klik Tampilkan Form
+  if (type === "luas") {
+    formLuas.classList.remove("hidden");
+    formKeliling.classList.add("hidden");
+  } else {
+    formKeliling.classList.remove("hidden");
+    formLuas.classList.add("hidden");
+  }
+}
